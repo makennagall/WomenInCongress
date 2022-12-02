35b20d6adffaa1b208e37e59117da9599f772423
makennagall <68980127+makennagall@users.noreply.github.com>
2022-12-02 15:07:37 -0500

    Standardize Column Names

--- WomenInCongress.R
+++ WomenInCongress.R
@@ -13,92 +13,81 @@ library(dplyr)
 library(lubridate)
 library(forcats)
 
-WomenPerSession <- read.csv("https://raw.githubusercontent.com/makennagall/WomenInCongress/main/WomenPerSession.csv")
+#Old Bill Data:
 OldAllOutput <- read.csv("https://raw.githubusercontent.com/makennagall/WomenInCongress/main/OldData/AllOutputs.csv")
 OldAllBillsNumbers <- read.csv("https://raw.githubusercontent.com/makennagall/WomenInCongress/main/OldData/AllBillsNumbers.csv")
 OldtermsCount <- read.csv('https://raw.githubusercontent.com/makennagall/WomenInCongress/main/OldData/termsCount.csv')
+
+#New Bill Data:
 AllBillsNumbers <- read.csv("https://raw.githubusercontent.com/makennagall/WomenInCongress/main/NewBillNumbers.csv")
-AllBillsNumbers <- mutate(AllBillsNumbers, percentWomen = (YesBills/Total)* 100)
 Frequency <- read.csv('https://raw.githubusercontent.com/makennagall/WomenInCongress/main/frequency.csv')
 WomenPerSession <- read.csv('https://raw.githubusercontent.com/makennagall/WomenInCongress/main/WomenPerSession.csv')
 AllOutput <- read.csv('https://raw.githubusercontent.com/makennagall/WomenInCongress/main/compiledNewOutputs.csv')
 TermsCount <- read.csv('https://raw.githubusercontent.com/makennagall/WomenInCongress/main/termsCountNew.csv')
 
-colnames(OldAllOutput)
-colnames(OldAllBillsNumbers)
-colnames(TermsCount)
-View(TermsCount)
-colnames(WomenPerSession)
-colnames(AllOutput)
-AllOutput$Congress
+#Adjust/Combine Data:
+  #Change Congress to integer:
+  AllOutput$Congress <- as.integer(AllOutput$Congress)
+  #Add Percent Women to AllBillsNumbers:
+  AllBillsNumbers <- mutate(AllBillsNumbers, percentWomen = (YesBills/Total)* 100)
+  #change LatestAction Date to Date object:
+  AllOutput$date <- as.Date(AllOutput$LatestActionDate, format = "%m/%d/%y")
+  TermsCount$date <- as.Date(TermsCount$LatestActionDate, format = "%m/%d/%y")
+  #add a DayMonth variable that is only the day and month so they can be displayed on the y axis:
+  AllOutput <- mutate(AllOutput, DayMonth = format(as.Date(date), "%m-%d"))
+  TermsCount <- mutate(AllOutput, DayMonth = format(as.Date(date), "%m-%d"))
+  #join data:
+  joinedData <- left_join(x = AllOutput, y = WomenPerSession, by = "Congress")
+  termDataJoined <- left_join(x = TermsCount, y = WomenPerSession, by = "Congress")
+  BillNumbersAndWomenPerSesh <- left_join(x = WomenPerSession, y = AllBillsNumbers, by = "Congress")
 
-#add a date variable that contains day, month and year and is a Date object:
-AllOutput <- mutate(AllOutput, date = paste(LatestActionMonth,"/",LatestActionDay,"/",LatestActionYear, sep = ""))
-AllOutput$date <- as.Date(AllOutput$date, format = "%m/%d/%Y")
-#add a DayMonth variable that is only the day and month so they can be displayed on the y axis:
-AllOutput <- mutate(AllOutput, DayMonth = format(as.Date(date), "%m-%d"))
-
-#A graph with:
-#x axis: Congressional Session
-#y axis: percent of the bills written that contained a key term pertaining to women or other gender minorities
-#color: total number of women in congress for that session
-WomenPerSession$Congress <- as.integer(WomenPerSession$Congress)
-TermsCount$Congress <- as.integer(TermsCount$Congress)
-joinedData <- left_join(x = AllOutput, y = WomenPerSession, by = "Congress")
-joinedData$Congress <- as.integer(joinedData$Congress)
-termDataJoined <- left_join(x = TermsCount, y = WomenPerSession, by = "Congress")
-BillNumbersAndWomenPerSesh <- left_join(x = WomenPerSession, y = AllBillsNumbers, by = "Congress")
-plot_ly(data = BillNumbersAndWomenPerSesh, type = "bar",
+#Draw Plotly Graphs:
+billPercentPlot <- plot_ly(data = BillNumbersAndWomenPerSesh, type = "bar",
         x = ~Congress, y = ~percentWomen, color = ~Total.Women,
         hoverinfo = 'text',
-        text = ~paste("Total Bills: ", Total, "<br>", "Women in the Bills: ", YesBills, "<br>Women in Congress: ", Total.Women))%>%
-          layout(title = "Congress v. Percentage of Bills", yaxis = list(title = "Bills that Contain Terms Related to Women (Percent)"),
-          legend = list(title = list( text = "<br>Total<br>Women<br>")))
-#reformats the year to contain only the month and day, so it can be more easily displayed in a timeline format
+        text = ~paste("Total Bills: ", Total, "<br>", "Women in the Bills: ", YesBills, "<br>Women in Congress: ", Total.Women))
+billPercentPlot <- billPercentPlot %>% layout( 
+        legend = list(title = list( text = "<br>Total<br>Women<br>")),
+        title = "Congress v. Percentage of Bills", 
+        yaxis = list(title = "Bills that Contain Terms Related to Women (Percent)"))
 
-plot_ly(data = BillNumbersAndWomenPerSesh, type = "bar",
+billNumberPlot <- plot_ly(data = BillNumbersAndWomenPerSesh, type = "bar",
         x = ~Congress, y = ~YesBills, color = ~Total.Women,
         hoverinfo = 'text',
-        text = ~paste("Total Bills: ", Total, "<br>", "Women in the Bills: ", YesBills, "<br>Women in Congress: ", Total.Women))%>%
-  layout(title = "Congress v. Number of Bills", yaxis = list(title = "Bills that Contain Terms Related to Women"),
-         legend = list(title = list( text = "<br>Total<br>Women<br>")))
-#reformats the year to contain only the month and day, so it can be more easily displayed in a timeline format
+        text = ~paste("Total Bills: ", Total, "<br>", "Women in the Bills: ", YesBills, "<br>Women in Congress: ", Total.Women))
+billNumberPlot <- billNumberPlot %>% layout(
+        legend = list(orientation = 'h', title = list(text = 'Women<br>in Congress<br>'), 
+        title = "Congress v. Number of Bills", 
+        yaxis = list(title = "Bills that Contain Terms Related to Women")
+        ))
 
-plot_ly(data = BillNumbersAndWomenPerSesh, type = "bar",
+numWomenPlot <- plot_ly(data = BillNumbersAndWomenPerSesh, type = "bar",
         x = ~Congress, y = ~Total.Women, color = ~percentWomen,
         hoverinfo = 'text',
         text = ~paste("Women in the House: ", Women.in.the.House, "<br>", "Women in the Senate: ", Women.in.the.Senate))%>%
           layout(title = "Congress v. Women in Congress", yaxis = list(title = "Women in Congress"),
                  legend = list(title = list( text = "<br>Percent<br>of Bills<br>")))
 
-#A graph with:
-#x axis: Year of Latest Update
-#y axis: Month and Day of Latest Update
-#color: Party that sponsored the bill
-plot_ly(data = AllOutput, type = "scatter", mode = "markers",
+byPartyPlot <- plot_ly(data = AllOutput, type = "scatter", mode = "markers",
         x = ~LatestActionYear, y = ~DayMonth, color = ~SponsorParty, legendgrouptitle = "Party",
         hoverinfo = 'text',
         text = ~paste("Title:", Title, "<br>", "URL:", URL))
 
-#left: returns all cases from the left (x) data table regardless of whether it has a matching y variable
 
-
-plot_ly(data = joinedData, type = "scatter", mode = "markers",
+indBillsCongressionalWomen <- plot_ly(data = joinedData, type = "scatter", mode = "markers",
         x = ~Congress, y = ~DayMonth, color = ~Total.Women,
         hoverinfo = 'text',
         text = ~paste("Title:", Title, "<br>", URL, "<br>Total Women: ", Total.Women, "<br>Sponsor: ", Sponsor, 
                       "<br>Sponsor Party: ", SponsorParty))
 
-#add a date variable that contains day, month and year and is a Date object:
-
-View(termDataJoined)
 plot_ly(data = termDataJoined, 
         type = "scatter", 
         mode = "markers",
-        x = ~term, 
+        x = ~Term, 
         y = ~Congress, 
         hoverinfo = 'text',
-        text = ~paste("Title:", title, "<br>", URL, "<br>Total Women: ", Total.Women, "<br>Sponsor: ", Sponsor))
+        text = ~paste("Title:", Title, "<br>", URL, "<br>Total Women: ", Total.Women, "<br>Sponsor: ", Sponsor, 
+                      "<br>Sponsor Party: ", Sponsor.Party))
 
 plot_ly(data = Frequency, 
         type = 'bar', 
@@ -108,4 +97,15 @@ plot_ly(data = Frequency,
         hoverinfo = 'text',
         text = ~paste("Term:", Term, "<br>Frequency", Frequency))
 
+plot_ly(data = termDataJoined, type = "scatter", mode = "markers",
+        x = ~LatestActionYear, y = ~DayMonth, color = ~Term,
+        hoverinfo = 'text',
+        text = ~paste("Title:", Title, "<br>", URL, "<br>Total Women: ", Total.Women, "<br>Sponsor: ", Sponsor, 
+                      "<br>Sponsor Party: ", Sponsor.Party))
 
+#Make Plots:
+billPercentPlot
+billNumberPlot
+numWomenPlot
+byPartyPlot
+indBillsCongressionalWomen
